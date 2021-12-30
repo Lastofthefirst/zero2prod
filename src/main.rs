56f6839f5f7fbbac0ba82ca239a5292614a18fd1
Quddus George <lastof@pm.me>
2021-12-30 06:39:43 -0500

    postrgres connection test

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::net::TcpListener;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
-
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("Failed to read configuration.");
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
     let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to listen port...");
