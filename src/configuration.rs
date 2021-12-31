83687c3397d0e07ec0bac4074e452fef3abbd4ae
Quddus George <lastof@pm.me>
2021-12-30 22:13:27 -0500

    logging

--- src/configuration.rs
+++ src/configuration.rs
@@ -32,3 +32,11 @@ impl DatabaseSettings {
         )
     }
 }
+impl DatabaseSettings {
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
+}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,8 @@
 use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use log;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -6,6 +10,29 @@ pub struct FormData {
     name: String,
 }
 
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
+pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    log::info!("request_id {} - Adding '{}' '{}' as a new subscriber.", request_id,form.email, form.name);
+    match sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now(),
+    )
+    .execute(pool.get_ref())
+    .await
+    {
+        Ok(_) => {
+            log::info!("request_id {} - New subscriber details have been saved.", request_id);
+            HttpResponse::Ok().finish()
+        }
+        Err(e) => {
+            log::error!("request_id {} - Failed to execute query: {:?}",request_id, e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
